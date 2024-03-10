06c11de934fbb08f45e97ae9273e70d353a420f4
LucasDev <LucasDev9645@gmail.com>
2024-03-10 11:03:04 -0300

    Restaurant registration page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+
     children: [
       {
         path: "/",
@@ -16,14 +18,17 @@ export const router = createBrowserRouter([
       },
     ],
   },
-
   {
-    path: "/sign-in",
+    path: "/",
     element: <AuthLayout />,
     children: [
       {
         path: "/sign-in",
         element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
       },
     ],
   },
